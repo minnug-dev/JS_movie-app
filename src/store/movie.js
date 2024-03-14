c35cc2c2fe58444f941f6bb131cafcafde407f8f
minnug <parkminnug@gmail.com>
2024-03-14 22:22:38 +0900

    ✨ 영화 목록 더 가져오기 - MovieListMore 컴포넌트 구현

--- src/store/movie.js
+++ src/store/movie.js
@@ -3,19 +3,21 @@ import { Store } from "../core/heropy"
 const store = new Store({
   searchText: '',
   page: 1,
+  pageMax: 1,
   movies: []
 })
 
 export default store 
 export const searchMovies = async page => {
+  store.state.page = page
   if (page === 1) {
-    store.state.page = 1
     store.state.movies = []
   }
   const res = await fetch(`https://omdbapi.com?apikey=dc015b69&s=${store.state.searchText}&page=${page}`)
-  const { Search } = await res.json()
+  const { Search, totalResults } = await res.json()
   store.state.movies = [
     ...store.state.movies, // page 불러오기
     ...Search 
   ]
+  store.state.pageMax = Math.ceil(Number(totalResults) / 10)
 }
