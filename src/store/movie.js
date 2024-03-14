978c729b05def8359a61762b4a30402f2e852148
minnug <parkminnug@gmail.com>
2024-03-14 18:57:11 +0900

    🔨 영화 검색 후 영화 목록 화면에 출력 구현 - MovieList 컴포넌트

--- src/store/movie.js
+++ src/store/movie.js
@@ -8,7 +8,14 @@ const store = new Store({
 
 export default store 
 export const searchMovies = async page => {
+  if (page === 1) {
+    store.state.page = 1
+    store.state.movies = []
+  }
   const res = await fetch(`https://omdbapi.com?apikey=dc015b69&s=${store.state.searchText}&page=${page}`)
-  const json = await res.json()
-  console.log(json)
+  const { Search } = await res.json()
+  store.state.movies = [
+    ...store.state.movies, // page 불러오기
+    ...Search 
+  ]
 }
