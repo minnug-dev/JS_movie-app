4c51618a2b18a499b78868674dd99447a22c58d8
minnug <parkminnug@gmail.com>
2024-03-15 17:51:31 +0900

    ✨ 내 정보 페이지 구현 - About 컴포넌트

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy"
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'MINNUG / ParkMinJoo',
+  email: 'parkminnug@gmail.com',
+  blog: 'https://velog.io/@minnug/posts',
+  github: 'https://github.com/minnug-dev',
+  repository: 'https://github.com/minnug-dev/movie-app-vaillajs'
+})
