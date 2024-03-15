2b4a99acdbff51516098dfee18f827a5f5a290a5
minnug <parkminnug@gmail.com>
2024-03-15 11:31:46 +0900

    🔨 영화 상세 페이지 상태 관리

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import { createRouter } from '../core/heropy'
 import Home from './Home'
+import Movie from './Movie'
 
 export default createRouter([
-  { path: '#/', component: Home } // 메인페이지
+  { path: '#/', component: Home }, // 메인페이지
+  { path: '#/movie', component:  Movie}
 ])
