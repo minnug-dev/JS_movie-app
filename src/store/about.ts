8656c87c712fafaba405d958cd9da8acdaaa29b5
minnug <parkminnug@gmail.com>
2024-03-28 15:44:39 +0900

    🏷️ about.ts 스토어 타이핑

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,10 +1,19 @@
 import { Store } from "../core/heropy"
 
-export default new Store({
+interface State {
+  photo: string
+  name: string
+  email: string
+  blog: string
+  github: string
+  repository: string
+}
+
+export default new Store<State>({
   photo: 'https://heropy.blog/css/images/logo.png',
   name: 'MINNUG / ParkMinJoo',
   email: 'parkminnug@gmail.com',
   blog: 'https://velog.io/@minnug/posts',
   github: 'https://github.com/minnug-dev',
-  repository: 'https://github.com/minnug-dev/movie-app-vaillajs'
+  repository: 'https://github.com/minnug-dev/movie-app-vaillajs',
 })
